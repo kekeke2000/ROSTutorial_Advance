5a6ee2afc6d6ebc1d4832a649cb6557a44b6d030
DreamWaterFound <1337841346@qq.com>
2022-06-28 00:38:46 +0800

    init repo

---
+++ ReadMe.md
@@ -0,0 +1,98 @@
+# ROS Tutorial Advance
+
+进阶一些的教程，主要目的：
+
+- 结合实践，尝试利用 ROS 下的各种概念、机制解决一些实际问题；
+- 熟悉 ROS 下和 OpenCV、PCL 等库的操作；
+- 通过代码了解一下你们的基本技能掌握情况；
+- 利用这个仓库演习一下多人协作。
+
+main 分支中是 需要补充的源码。
+
+操作过程：
+
+- 先同意我发出的仓库协作邀请（当看到这个文档的时候你们就已经完成了）；
+- 点右上角的 Fork 按钮，将这个仓库 Fork 到你们自己的账户下；
+
+![fork](./doc/Images/1.png)
+
+- 创建一个专门的 ROS 工作空间，在**自己**账号下的该代码仓库中，git clone 到自己的电脑上. 例如：
+```bash
+# 创建工作空间，这里放在 ~/ROSTutorial_ws 下，你可以放在任何你想放在的地方
+$ cd ~
+$ mkdir ROSTutorial_ws
+$ cd ROSTutorial_ws
+# 这里的链接在 Github 上可以直接生成，看下图
+$ git clone https://github.com/xxx/ROSTutorial_Advance.git
+# 其实这个仓库就是 ROS 工作空间下 src 文件夹的内容，咱们直接修改为 src
+$ mv ./ROSTutorial_Advance ./src   
+# 初始化 ROS 工作空间
+$ cd src && catkin_init_workspace
+# 进行编译
+$ catkin_make 
+
+
+
+TODO 补充
+
+```
+
+  > 注意使用 git clone 的方式，不要下载 zip 的方式，这种方式只有代码本身，不会包含代码仓库信息
+- 在**自己**的电脑上新建自己命名的分支（branch），**务必确保**在**自己**的分支下操作和处理代码；
+- 看讲义[0](./doc/Guides/0、Turtlebot3-SLAM与导航虚拟仿真实验-课前准备.pdf)、[1](./doc/Guides/1、Turtlebot3-SLAM与导航虚拟仿真实验-讲义.pdf)，跟着做基础的实验；
+- 跟着讲义完成教程、补全代码、完成预期功能；
+- 现阶段你们在做实验的过程中，有困难或者吃力的感觉是正常的，有什么不懂或者疑惑的随时问我；
+- 每当做出了关键性的进展要**及时提交**(commit)到本地代码仓库，并且**及时同步**到自己账号下Github的代码仓库；这是避免因为误删等意外原因，及时止损的关键办法，要养成及时commit的习惯；
+- 每完成一个功能包的代码补全，确保本地代码同步到**自己**的Github仓库后，提交一个 Merge 请求到**这个**仓库和你们自己名字相关的branch下，由师兄来负责 Code review 和代码合并。
+
+## 一些注意事项
+
+- 讲义中所有关于git的操作不要执行，会搞乱
+- 这样子的注释表示，这里需要补充的代码对应哪个章节的实验：
+
+TODO 补充图像
+
+- 3.5 节提到的实验不需要完成
+
+## 变量命名规则
+
+继承了匈牙利命名法的大部分特点. 这种命名方法看上去十分冗长, 但是不仅可以做到"见名知义",还可"见名知类型".
+同学们自己写程序的时候还是以实现功能为第一要务, 不必遵循程序中的命名规则.
+对于非数学对象, 变量名主要由: 作用域 + 类型 + 描述 组成. 如 `ExperNodeBase.hpp` 中的 `ExperNodeBase::mupNodeHandle`:
+ - m:   member, 作用域, 表示这个变量是类的成员变量
+ - up:  unique_ptr, 类型, 表示这个变量是 std::unique_ptr 型变量
+ - NodeHandle: 描述, 遵循驼峰命名法, 表示这个变量和节点句柄相关
+
+作用域一般有这几类:
+ - m: member, 类的成员变量
+ - l: local,  程序中定义的局部变量, 但是一般这个前缀不加
+ - g: global, 全局变量
+ 
+常用的类型有这几类:
+基本类型:
+ - n: number, int, unsigned int, long, uint64_t, size_t 等等表示整数的变量
+ - c: 特指 char, unsigned char
+ - f: float型浮点数
+ - d: double 型浮点数
+ - e: enum, 枚举变量
+ - p: 指针, 通常与上述搭配, 如 pdMax 表示指向最大值的指针变量, 指向的变量是 double 类型
+标准库类型:
+ - str: string, 字符串, 通常指代 std::string
+ - v:   vector, 通常指代 std::vector
+ - map: map,    键值对映射表, 通常指代 std::map 一族
+ - list:list,   列表, 通常指代 std::list
+ - a:   array,  通常指代 boost::array 和 std::array,; 传统 C 风格数组使用 p 表示
+ - sp:  shared_ptr, 共享指针, 通常指代 boost::shared_ptr 和 std::shared_ptr
+ - up:  unique_ptr, 不知道咋说, 通常指代 boost::unique_ptr 和 std::unique_ptr
+
+方便起见,对于指针和带有模板参数的基本类型和标准库类型可以"套娃":
+```C++
+
+std::vector<size_t> vnVar1;
+std::unique_ptr<std::vector<float> > upvfVar2;
+std::vector<std::vector<int*> >      vvpnVar3;
+std::vector<std::vector<std::vector<float> > >
+                                     vvvfVar3;
+```
+
+
---
+++ octo_mapping/src/ImageBase/ImageBase.cc
@@ -0,0 +1,284 @@
+/**
+ * @file ImageBase.cc
+ * @author guoqing (1337841346@qq.com)
+ * @brief 基本图像的获取, 处理和发布
+ * @version 0.1
+ * @date 2020-06-04
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+/* ========================================== 头文件 =========================================== */
+// C++ STL
+#include <string>
+#include <iostream>
+
+// ROS
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+// TODO 补充
+
+// OpenCV
+#include <opencv2/opencv.hpp>
+
+// 节点基类
+#include "ExperNodeBase.hpp"
+// 相机内参
+#include "CameraIntrinsic.hpp"
+
+
+/* ========================================== 宏定义 =========================================== */
+// 原图像 topic
+#define MACRO_SRC_RGB_IMG_TOPIC                 "/camera/rgb/image_raw"
+#define MACRO_SRC_DEPTH_IMG_TOPIC               "/camera/depth/image_raw"
+
+// 相机参数 topic
+#define MACRO_SRC_RGB_IMG_INFO_TOPIC            "/camera/rgb/camera_info"
+#define MACRO_SRC_DEPTH_IMG_INFO_TOPIC          "/camera/depth/camera_info"
+
+// 图像缩小倍数的逆
+#define CONST_IMAGE_DEFAULT_SCALE_FACTOR_INV    0.25
+
+/* ========================================== 程序正文 =========================================== */
+class ImageBaseNode : public ExperNodeBase
+{
+public:
+    /**
+     * @brief 构造函数
+     * @param[in] nArgc         命令行参数个数
+     * @param[in] ppcArgv       命令行参数表列
+     * @param[in] pcNodeName    当前节点名称
+     */
+    ImageBaseNode(int nArgc, char** ppcArgv, const char* pcNodeName)
+        : ExperNodeBase(nArgc, ppcArgv, pcNodeName)
+    {
+        // 生成
+        mupImgTrans.reset(new image_transport::ImageTransport(*mupNodeHandle));
+
+        // 订阅彩色图像, 队列长度最好为1 
+        mImgSubRGB = mupImgTrans->subscribe(MACRO_SRC_RGB_IMG_TOPIC, 1, 
+                            boost::bind(&ImageBaseNode::ImageRGBCallback, this, _1));
+
+        // 这种写法也是可以的, 注意设置类的成员函数作为回调函数时, 需要传递 this 指针
+        // mImgSubRGB = mupImgTrans->subscribe(MACRO_SRC_RGB_IMG_TOPIC, 1, this);
+
+        // TODO 3.2.1 订阅深度图像
+        // mImgSubDepth = _____;
+
+        // 发布
+        mImgPubColoredDepth = mupImgTrans->advertise(MACRO_SRC_COLORED_DEPTH_RES_TOPIC, 1);
+
+
+        // 订阅彩色相机内参
+        mSubRGBCamInfo = mupNodeHandle->subscribe<sensor_msgs::CameraInfo>(MACRO_SRC_RGB_IMG_INFO_TOPIC, 1, 
+            boost::bind(&ImageBaseNode::RGBCamInfoCallBack, this, _1));
+        
+        // TODO 订阅深度相机内参
+        mSubDepthCamInfo = mupNodeHandle->subscribe<sensor_msgs::CameraInfo>(MACRO_SRC_DEPTH_IMG_INFO_TOPIC, 1, 
+            boost::bind(&ImageBaseNode::DepthCamInfoCallBack, this, _1));
+
+
+        // 从参数服务器获取参数
+        mupNodeHandle->param<float>("/image_base/scale_factor_inv",         // 名
+                                    mfSCaleFactorInv,                       // 接收变量
+                                    CONST_IMAGE_DEFAULT_SCALE_FACTOR_INV);  // 默认值
+
+        // 延时, 避免出现幺蛾子
+        ros::Duration(0.1).sleep();
+        ROS_INFO("Initialized, scaled factor inverse = %.4f", mfSCaleFactorInv);
+
+    }
+
+    /** @brief 析构函数 */
+    ~ImageBaseNode() {}  
+
+    /** @brief 主循环 */
+    void Run(void) override
+    {
+        // TODO 3.2.1 
+        // ______;
+    }
+
+public:
+
+    /**
+     * @brief 彩色图像到来时的回调函数
+     * @param[in] msg 消息指针
+     */
+    void ImageRGBCallback(const sensor_msgs::ImageConstPtr& pMsg)
+    {
+        // 生成图像
+        cv::Mat imgRecv;
+        try
+        {
+            // 利用 cv_bridge 转换, OpenCV彩色图像格式为 BGR 通道顺序, 每通道8bit
+            imgRecv = cv_bridge::toCvShare(pMsg, "bgr8")->image;
+        }
+        catch (cv_bridge::Exception& e)
+        {
+            // 如果出现问题
+            ROS_ERROR("Could not convert from '%s' to 'bgr8'.", pMsg->encoding.c_str());
+        }
+
+        // 大小缩放
+        cv::resize(imgRecv, mimgScaledLastRGB, cv::Size(), 
+                    mfSCaleFactorInv, mfSCaleFactorInv);
+
+        // TODO 3.2.2 彩色图像灰度化并发布图像
+        // cv::cvtColor(_____);
+        // _______;
+
+
+        // 显示图像
+        cv::imshow("RGB Image", mimgScaledLastRGB);
+        // 注意必须调用它,否则显示图像的 OpenCV 窗口不会刷新
+        cv::waitKey(1);
+    }
+
+
+    /**
+     * @brief 深度图像到来时的回调函数
+     * @param[in] pMsg 
+     */
+    void ImageDepthCallback(const sensor_msgs::ImageConstPtr& pMsg)
+    {
+        cv::Mat imgRecv;
+        
+        try
+        {
+            // 这里会自动重载
+            // _____ = cv_bridge::toCvShare(pMsg)->image ; 
+        }
+        catch (cv_bridge::Exception& e)
+        {
+            ROS_ERROR("Could not convert depth image.");
+        }
+
+        // TODO 3.2.1 缩放图像
+        // __________;
+
+        
+        // TODO 3.2.2 调用伪彩色化函数
+        // 根据需要自由补充代码
+        // 调用如下函数实现深度度向彩色图的转换
+        // ColoredDepth(/*深度图*/______, /*转换后图像, 是彩色图*/___________);
+
+        // TODO 3.2.2 发布图像
+        // 消息类型转换
+        // _____ ________ = _____::______(std_msgs::Header(), "bgr8", _______).toImageMsg();
+        // 发布消息
+        // _____.publish(_____);
+        
+        // TODO 3.2.1 显示图像
+        // _______________
+        // ....
+    }
+
+
+    /**
+     * @brief RGB相机参数到来时的回调函数
+     * @param[in] pMsg 消息指针
+     */
+    void RGBCamInfoCallBack(const sensor_msgs::CameraInfo::ConstPtr& pMsg)
+    {
+        // 注意内参的变化, 这里需要计算缩小后图像的等效相机内参
+        mRGBCameraIntrinsic.fx = pMsg->K[0] * mfSCaleFactorInv;
+        mRGBCameraIntrinsic.fy = pMsg->K[4] * mfSCaleFactorInv;
+
+        mRGBCameraIntrinsic.cx = pMsg->K[2] * mfSCaleFactorInv;
+        mRGBCameraIntrinsic.cy = pMsg->K[5] * mfSCaleFactorInv;
+
+        mRGBCameraIntrinsic.isOK = true;
+
+        ROS_INFO_STREAM("RGB camera intrinsic: fx = " << mRGBCameraIntrinsic.fx <<
+                        ", fy = " << mRGBCameraIntrinsic.fy << 
+                        ", cx = " << mRGBCameraIntrinsic.cx << 
+                        ", cy = " << mRGBCameraIntrinsic.cy);
+
+        // 关闭订阅器
+        mSubRGBCamInfo.shutdown();
+
+    }
+
+    /**
+     * @brief 深度相机参数到来时的回调函数
+     * @param[in] pMsg 
+     */
+    void DepthCamInfoCallBack(const sensor_msgs::CameraInfo::ConstPtr& pMsg)
+    {
+        // TODO 3.2.3 补充代码
+    }
+
+private:
+
+    /**
+     * @brief 深度图像转换成伪彩色图像
+     * @param[in]  imgSrc 输入的深度图像
+     * @param[out] imgDst 输出的伪彩色图像
+     */
+    void ColoredDepth(const cv::Mat& imgSrc, cv::Mat& imgDst)
+    {
+        cv::Mat imgAbs;
+        // 数值映射
+        cv::convertScaleAbs(imgSrc, imgAbs, 51.2, 0.0);
+        // 转换为伪彩色图
+        cv::applyColorMap(255 - imgAbs, imgDst, cv::COLORMAP_JET);
+
+        size_t nRows = imgSrc.rows;
+        size_t nCols = imgSrc.cols;
+
+        cv::Vec3b cvBlack(0, 0, 0);
+
+        // 处理一些特殊的 nan 像素
+        for(size_t nIdY = 0; nIdY < nRows; ++nIdY)
+        {
+            for(size_t nIdX = 0; nIdX < nCols; ++nIdX)
+            {
+                // 深度值无效的点在深度图中以 nan 表示, 这里特殊处理一下, 作为黑色显示
+                if(std::isnan(imgSrc.at<float>(nIdY, nIdX)))
+                {
+                    imgDst.at<cv::Vec3b>(nIdY, nIdX) = cvBlack;
+                }
+            }
+        }
+    }
+
+private:
+
+    std::unique_ptr<image_transport::ImageTransport> mupImgTrans;               ///< ImageTransport 对象指针
+
+    ros::Subscriber                                  mSubRGBCamInfo;            ///< RGB相机参数订阅器
+    ros::Subscriber                                  mSubDenizpthCamInfo;       ///< 深度相机参数订阅器
+
+    // HERE
+
+    image_transport::Subscriber                      mImgSubRGB;                ///< 彩色图像订阅器
+    // TODO 3.2.1
+    //_______________::__________                    mImgSubDepth;              ///< 深度图像订阅器
+    // TODO 3.2.2 添加发布器
+    // ... 随意添加
+
+    CameraIntrinsic                                  mRGBCameraIntrinsic;       ///< 彩色相机内参
+    CameraIntrinsic                                  mDepthCameraIntrinsic;     ///< 深度相机内参
+
+    cv::Mat                                          mimgScaledLastRGB;         ///< 缩放后的彩色图像
+    cv::Mat                                          mimgScaledLastDepth;       ///< 缩放后的深度图像
+
+    float                                            mfSCaleFactorInv;          ///< 缩放因子
+
+};
+
+/**
+ * @brief 主函数
+ * @param[in] argc 命令行参数个数
+ * @param[in] argv 命令行参数表列
+ * @return int 退出状态
+ */
+int main(int argc, char **argv)
+{
+    ImageBaseNode node(argc, argv, "image_base");
+    node.Run();
+    return 0;
+}
